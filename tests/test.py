e98a4a6666fce549965e27913c4905ce4e4f1f88
yuying.chen <yuying.chen@tum.de>
2026-06-02 22:33:29 +0200

    "add test file:test preprocessor"

--- tests/test.py
+++
@@ -1,9 +0,0 @@
-from src.config import INPUT_PDF_DIR
-from src.preprocessing.mistral.mistral_client import run_mistral_ocr
-from src.preprocessing.models.pages import pages_from_ocr
-
-
-file_path =INPUT_PDF_DIR/"ITG-3050-Register-Map.pdf"
-ocr_data= run_mistral_ocr(file_path)
-pages = pages_from_ocr(ocr_data)
-
