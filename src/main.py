7586b6536ec1af7200ecb92a5de2c4a0a4389130
yuying.chen <yuying.chen@tum.de>
2026-06-04 22:11:09 +0200

    "update main"

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
-from tests.draft import test
+
+from tests.test_without_preprocessing import test_without_preprocessing
+from tests.test_preprocessor import test_preprocessor_pipeline
 
 if __name__ == '__main__':
-    test()
+    name = "MCP9808.pdf"
+    test_without_preprocessing(name)
+    test_preprocessor_pipeline(name)
+
 
 
