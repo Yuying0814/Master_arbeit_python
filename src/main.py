619a547a66d98f548ddb61319c6d46cb780b35f2
yuying.chen <yuying.chen@tum.de>
2026-06-14 04:02:10 +0200

    "update main"

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import sys
 from pathlib import Path
+from typing import Literal
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(PROJECT_ROOT))
@@ -11,18 +12,32 @@ from tests.test_preprocessor import test_preprocessor_pipeline
 
 
 if __name__ == "__main__":
-    input_pdf_dir = PROJECT_ROOT / "data" / "input_pdf"
+    mode: Literal["single_test","multiple_test"] = "single_test"
+    match mode:
+        case "single_test":
+            pdf_file = PROJECT_ROOT / "tests" / "lis3dh.pdf"
+            pdf_name = pdf_file.name
 
-    pdf_files = sorted(input_pdf_dir.glob("*.pdf"))
+            print(f"\n==============================")
+            print(f"Running tests for: {pdf_name}")
+            print(f"==============================\n")
 
-    for pdf_path in pdf_files:
-        pdf_name = pdf_path.name
+            test_without_preprocessing(pdf_name)
+            test_preprocessor_pipeline(pdf_name)
 
-        print(f"\n==============================")
-        print(f"Running tests for: {pdf_name}")
-        print(f"==============================\n")
+        case "multiple_test":
+            input_pdf_dir = PROJECT_ROOT / "data" / "input_pdf"
 
-        test_without_preprocessing(pdf_name)
-        test_preprocessor_pipeline(pdf_name)
+            pdf_files = sorted(input_pdf_dir.glob("*.pdf"))
+
+            for pdf_path in pdf_files:
+                pdf_name = pdf_path.name
+
+                print(f"\n==============================")
+                print(f"Running tests for: {pdf_name}")
+                print(f"==============================\n")
+
+                test_without_preprocessing(pdf_name)
+                test_preprocessor_pipeline(pdf_name)
 
 
