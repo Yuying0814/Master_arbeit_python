a5ced4a6f0f417e9bff4d12f3d2f647158732a73
yuying.chen <yuying.chen@tum.de>
2026-06-19 00:52:41 +0200

    "update"

--- src/coding/coder/file_coder.py
+++
@@ -1,6 +0,0 @@
-from src.models.coder import FileCoderInput
-
-class FileCoder:
-
-    def __init__(self,api_key):
-        pass
--- src/coding/coder/file_planner.py
+++
@@ -1,5 +0,0 @@
-from src.models.coder import FilePlannerInput
-
-class FilePlanner:
-    def __init__(self, api_key):
-        pass
