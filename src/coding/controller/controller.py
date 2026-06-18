4e2874e129b1dbb54611056e2370c430815619a7
yuying.chen <yuying.chen@tum.de>
2026-06-19 00:53:37 +0200

    "Finish controller"

--- src/coding/controller/controller.py
+++ src/coding/controller/controller.py
@@ -1,79 +1,147 @@
+from typing import Any
 
 from src.coding.config import CodingConfig
-from src.coding.verifier.verifier import Verifier
 from src.coding.planner.planner import Planner
-from src.coding.retriever.retriever import Retriever
-from src.models.controller import WorkFlowLog
-from src.models.coder import CodeFile, CoderInput
+from src.coding.retriever.retriever import PageRetriever
 from src.coding.coder.coder import Coder
 from src.coding.filewriter.filewriter import FileWriter
+from src.coding.verifier.verifier import Verifier
+
+from src.models.coding_common import ProgrammingPlan,VerificationPlan,CodeFile
+from src.models.register_output import RegisterMapOutput
+from src.models.planner import PlannerInput,PlannerOutput
+from src.models.retriever import RetrievalResult
+from src.models.coder import CoderInput,CoderOutput
+from src.models.verifier import VerifierInput,VerifierOutput
 
 class Controller:
-    user_request:str = ""
-    logs: list[WorkFlowLog]
+    candidate_files: list[CodeFile]
     accepted_files: list[CodeFile]
-    not_accepted_files: list[CodeFile]
     max_tries: int = 10
+    logs: list
     config:CodingConfig
 
-    def __init__(self,user_request,config:CodingConfig):
-        self.user_request = user_request
+    def __init__(self,config:CodingConfig,pages:list[dict[str,Any]],register_map:RegisterMapOutput):
         self.logs = []
+        self.candidate_files = []
         self.accepted_files = []
         self.config = config
         self.not_accepted_files = []
 
-    def main(self):
-        planner = FakePlanner(self.config.get_apikey("openai"))
-        retriever = FakeRetriever(self.config.get_apikey("openai"))
+        self.pages = pages
+        self.register_map = register_map
 
-        planner_output = planner.create_plan(self.user_request)
-        programming_plans = planner_output.ProgrammingPlans
-        retrieval_requests = planner_output.RetrievalRequests
+        self._load_agents()
 
-        if len(programming_plans) != len(retrieval_requests):
-            raise ValueError(
-                "Each programming plan must have exactly one retrieval request. "
-                "Use an empty retrieval request when no retrieval is needed."
-            )
+        self._check_valid_client()
+        self._check_valid_fqbn()
 
-        #
-        for programming_plan,retrieval_request in zip(programming_plans, retrieval_requests):
-            coder = FakeCoder(self.config.get_apikey("openai"))
-            verifier = FakeVerifier(self.config.get_apikey("openai"))
+    async def run(self,user_request:str = None):
+        verifier_feedback = None
 
-            retrieval_result = retriever.run(retrieval_request)
-            coder.start_plan(
-                CoderInput(
-                    programming_plan=programming_plan,
-                    retrieval_result=retrieval_result,
-                    accepted_files=self.accepted_files
-                )
-            )
+        for attempt in range(1,self.max_tries+1):
 
-            for attempt in range(1,self.max_tries+1):
-                coder.run()
+            planner_input = self._build_planner_input(user_request,verifier_feedback)
+            planner_output = self.planner.create_plan(planner_input)
 
-                verification_request = coder.send_verification_request()
+            programming_plan = planner_output.programming_plan
+            topics = planner_output.retrieval_topics
+            verification_plan = planner_output.verification_plan
 
-                verifier.receive_request(verification_request)
+            retrieval_response = await self.retriever.run(topics)
+            retrieval_results = retrieval_response.results
 
-                verifier.run()
+            coder_input = self._build_coder_input(programming_plan,retrieval_results)
+            coder_output = self.coder.run(coder_input)
 
-                feedback = verifier.get_feedback()
+            self._update_candidate_files(coder_output)
 
-                coder.receive_feedback(feedback)
-                if feedback.passed:
-                    break
+            verifier_input = self._build_verifier_input(verification_plan,retrieval_results)
+            verifier_output = self.verifier.run(verifier_input)
 
-            coder_output = coder.get_output()
-            if coder_output.passed:
+            if verifier_output.passed:
                 self.accepted_files.extend(coder_output.files)
+                self._update_logs()
+                break
             else:
-                self.not_accepted_files.extend(coder_output.files)
+                self.candidate_files.extend(coder_output.files)
+            verifier_feedback = verifier_output
 
-        FileWriter.write_to_files(self.accepted_files,self.config.project_path.code_dir)
+        if len(self.accepted_files) > 0:
+            FileWriter.write_to_files(self.accepted_files,self.config.project_path.code_dir)
 
 
-    def receive_user_request(self,user_request:str):
-        self.user_request = user_request
+
+    def _load_agents(self):
+        task_configs = self.config.task_configs
+        self.planner = Planner.load_from_task_config(
+            task_config=task_configs.planning,
+            api_key=self.config.get_apikey(task_configs.planning.model.provider),
+            tools=None,
+        )
+
+        self.retriever = PageRetriever.load_from_task_config(
+            pages=self.pages,
+            task_config=task_configs.retrieval,
+            api_key=self.config.get_apikey(task_configs.retrieval.model.provider),
+            input_path=self.config.project_path.input_path / "retrieval.jsonl"
+        )
+
+        self.coder = Coder.load_from_task_config(
+            task_config=task_configs.coding,
+            api_key=self.config.get_apikey(task_configs.coding.model.provider),
+            tools=None,
+        )
+
+        self.verifier = Verifier
+
+    def _build_planner_input(self,user_request:str|None,verifier_feedback:VerifierOutput|None) -> PlannerInput:
+        if user_request is None:
+            user_request = "Generate C driver"
+
+        return PlannerInput(
+            user_request=user_request,
+            register_map=self.register_map,
+            candidate_files=self.candidate_files,
+            accepted_files=self.accepted_files,
+            verifier_feedback=verifier_feedback,
+        )
+
+    def _build_coder_input(self,programming_plan:ProgrammingPlan,retrieval_results:list[RetrievalResult]) -> CoderInput:
+        return CoderInput(
+            programming_plan=programming_plan,
+            register_map=self.register_map,
+            retrieval_result=retrieval_results,
+            candidate_files=self.candidate_files,
+            accepted_files=self.accepted_files,
+        )
+
+    def _build_verifier_input(self,verification_plan:VerificationPlan,retrieval_results:list[RetrievalResult]) -> VerifierInput:
+        return VerifierInput(
+            verification_plan=verification_plan,
+            register_map=self.register_map,
+            retrieval_results=retrieval_results,
+            candidate_files=self.candidate_files,
+            accepted_files=self.accepted_files,
+        )
+
+    def _update_candidate_files(self, coder_output:CoderOutput) -> None:
+        files = coder_output.candidate_files
+        candidate_files_by_id = {
+            candidate_file.file_id: candidate_file
+            for candidate_file in self.candidate_files
+        }
+
+        for code_file in files:
+            candidate_files_by_id[code_file.file_id] = code_file
+
+        self.candidate_files = list(candidate_files_by_id.values())
+
+    def _update_logs(self) -> None:
+        pass
+
+    def _check_valid_client(self):
+        pass
+
+    def _check_valid_fqbn(self):
+        pass
