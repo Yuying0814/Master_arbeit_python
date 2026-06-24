ab024109b27ced283507adf2d29772c658932aa5
yuying.chen <yuying.chen@tum.de>
2026-06-24 16:55:08 +0200

    "update"

--- src/models/coding_common.py
+++ src/models/coding_common.py
@@ -6,7 +6,7 @@ from src.models.structuredOutputModel import StructuredOutputModel
 class IncompleteImplementation(StructuredOutputModel):
 
     target_section: str = Field(
-        description="Name of the incomplete target section.",
+        description="Name of the incomplete target section location.",
     )
 
     target_function: str = Field(
