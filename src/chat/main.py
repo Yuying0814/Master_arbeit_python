c55581fdc60ecbb477a2f847b0c6d5a3de0540a2
yuying.chen <yuying.chen@tum.de>
2026-08-07 09:01:02 +0200

    "update main"

--- src/chat/main.py
+++ src/chat/main.py
@@ -1,20 +1,27 @@
 from __future__ import annotations
 
+from src.coding.config import CodingConfig
+from src.preprocessing.config import PreprocessingConfig
 from src.chat.chat_agent import ChatAgent
 from src.chat.config import ChatConfig
 from src.chat.tools import QuitChatRequested, read_user_input
 
 
-def start_chat(config: ChatConfig | None = None) -> None:
-    config = config or ChatConfig.load_config()
+def start_chat(
+        chat_config: ChatConfig,
+        preprocessing_config: PreprocessingConfig,
+        coding_config: CodingConfig,
+) -> None:
 
     try:
-        agent = ChatAgent(config)
+        agent = ChatAgent(
+            chat_config=chat_config,
+            preprocessing_config=preprocessing_config,
+            coding_config=coding_config
+        )
     except Exception as error:
         print(f"Failed to start chat: {error}")
         return
-
-
 
     try:
         begin = True
@@ -22,8 +29,12 @@ def start_chat(config: ChatConfig | None = None) -> None:
         while True:
             count += 1
             if begin:
-                response = agent.run("Hi")
                 print("\n   Chat started  ✔ ")
+                try:
+                    response = agent.run("Hi")
+                except Exception as error:
+                    print(f"Failed to get initial response: {error}")
+                    return
                 print(f"======================== chat round{count} =========================")
                 print(f"Assistant: {response}")
                 print("(Type 'quit' to exit.)\n")
@@ -38,15 +49,8 @@ def start_chat(config: ChatConfig | None = None) -> None:
                 response = agent.run(user_input)
                 print(f"========================chat round{count}=========================")
                 print(f"Assistant: {response}")
-                print("(Type 'quit chat' to exit.)\n")
+                print("(Type 'quit' to exit.)\n")
             except Exception as error:
                 print(f"Assistant error: {error}")
     except (QuitChatRequested, EOFError, KeyboardInterrupt):
         print("\nChat closed.")
-
-
-def main() -> None:
-    start_chat()
-
-if __name__ == "__main__":
-    main()
--- src/main.py
+++ src/main.py
@@ -1,131 +1,317 @@
-import sys
+
 import asyncio
 from pathlib import Path
-from typing import Any,Literal
+from typing import Any
+
+
+from src.chat.config import ChatConfig
+from src.chat.main import start_chat
 from src.preprocessing.config import PreprocessingConfig
-from src.preprocessing.preprocessor import Preprocessor
+from src.coding.config import CodingConfig
+from src.workflow import run_preprocessor,run_coding_controller,get_latest_data
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
 # ============================================================
-# User configuration
+# MODE
 # ============================================================
-# Set MODE to "single_test" for one PDF or "multiple_test" for all PDFs in PDF_DIR.
-MODE: Literal["single_test", "multiple_test"] = "single_test"
+MODE = "chat" # chat/ single_file / multiple_files
 
-# Set the input PDF path for single_test mode.
+# ============================================================
+# General configuration for single_file/multiple_files
+# ============================================================
+DEVICE_NAME = ""
+DEVICE_NAMES = [""]
+USER_REQUEST = ""
+
+# ============================================================
+# Path configuration
+# ============================================================
 PDF_FILE: Path | str = PROJECT_ROOT / "data"/ "input_pdf" / "ITG-3050-Register-Map.pdf"
-
-# Set the input PDF directory for multiple_test mode.
 PDF_DIR: Path | str = PROJECT_ROOT / "data"/ "input_pdf"
-
-# Set the .env file path. Leave it as ".env" if the file is in the project root.
 ENV_FILE: Path | str = "D:/python/master_arbeit/.env"
+DATABASE_PATH: Path | str = "D:/python/master_arbeit/data/database.db"
+CODE_DIR: Path | str = "D:/python/master_arbeit/temp"
+CLI_PATH = PROJECT_ROOT/"arduino"/"bin"/"arduino-cli.exe"
 
 # ============================================================
 # Model configuration
 # ============================================================
-def configure_preprocessing_models(config: PreprocessingConfig) -> None:
-    """Configure model settings for the current preprocessing run."""
-    """Available settings for each task:
-        provider: str, "openai", "ollama"
-        is_batch: bool, True, False (better not change)
-        model_name: str, "gpt-5-mini", "gpt-5.4", ...
-        temperature: int
-        max_tokens: int
-    """
-    model_settings: dict[str, dict[str, Any]] = {
-        "classify_pages": {
-            "provider": "openai",
-            "is_batch": True,
-            "model_name": "gpt-5-mini",
-            "thinking_effort":"medium",
-            "temperature": 0.0,
-            "max_tokens": 10000,
-        },
-        "verify_reg_sum_pages": {
-            "provider": "openai",
-            "is_batch": True,
-            "model_name": "gpt-5-mini",
-            "thinking_effort": "medium",
-            "temperature": 0.0,
-            "max_tokens": 10000,
-        },
-        "verify_reg_pages": {
-            "provider": "openai",
-            "is_batch": True,
-            "model_name": "gpt-5-mini",
-            "thinking_effort": "medium",
-            "temperature": 0.0,
-            "max_tokens": 10000,
-        },
-        # "add_page_description": {
-        #     "provider": "openai",
-        #     "is_batch": True,
-        #     "model_name": "gpt-5-mini",
-        #     "temperature": 0.0,
-        #     "max_tokens": 2000,
-        # },
-        "extract_reg_index": {
-            "provider": "openai",
-            "is_batch": False,
-            "model_name": "gpt-5-mini",
-            "thinking_effort": "medium",
-            "temperature": 0.0,
-            "max_tokens": 20000,
-        },
-        "extract_reg_map": {
-            "provider": "openai",
-            "is_batch": False,
-            "model_name": "gpt-5.4",
-            "thinking_effort": "high",
-            "temperature": 0.0,
-            "max_tokens": 100000,
-        },
-    }
+PREPROCESSOR_TASK_MODEL_SETTINGS: dict[str, dict[str, Any]] = {
+    "ocr":{
+        "provider": "mistral",
+        "model_name": "mistral-ocr-latest",
+        "include_image": False,
+        "table_format": "html",
+        # Following settings are applied only for local glm-ocr
+        "url": "http://localhost:11434",
+        "layout_device": "cpu",
+        "connection_pool_size": 1,
+        "max_workers": 1,
+        "batch_size": 1,
+    },
+
+    "classify_pages": {
+        "provider": "openai",
+        "is_batch": True,
+        "model_name": "gpt-5-mini",
+        "thinking_effort": "medium",
+        "temperature": 0.0,
+        "max_tokens": 10000,
+    },
+    "verify_reg_sum_pages": {
+        "provider": "openai",
+        "is_batch": True,
+        "model_name": "gpt-5-mini",
+        "thinking_effort": "medium",
+        "temperature": 0.0,
+        "max_tokens": 10000,
+    },
+    "verify_reg_pages": {
+        "provider": "openai",
+        "is_batch": True,
+        "model_name": "gpt-5-mini",
+        "thinking_effort": "medium",
+        "temperature": 0.0,
+        "max_tokens": 10000,
+    },
+    "extract_reg_index": {
+        "provider": "openai",
+        "model_name": "gpt-5-mini",
+        "thinking_effort": "medium",
+        "temperature": 0.0,
+        "max_tokens": 20000,
+    },
+    "extract_reg_map": {
+        "provider": "openai",
+        "model_name": "gpt-5-mini",
+        "thinking_effort": "high",
+        "temperature": 0.0,
+        "max_tokens": 100000,
+    },
+}
+CODING_TASK_MODEL_SETTINGS: dict[str, dict[str, Any]] = {
+    "planning": {
+        "provider": "openai",
+        "model_name": "gpt-5-mini",
+        "thinking_effort": "high",
+        "temperature": 0.0,
+        "max_tokens": 100000,
+    },
+    "retrieval": {
+        "provider": "openai",
+        "model_name": "gpt-5-mini",
+        "thinking_effort": "high",
+        "temperature": 0.0,
+        "max_tokens": 5000,
+    },
+    "coding": {
+        "provider": "openai",
+        "model_name": "gpt-5-mini",
+        "thinking_effort": "high",
+        "temperature": 0.0,
+        "max_tokens": 20000,
+    },
+    "verification_semantic": {
+        "provider": "openai",
+        "model_name": "gpt-5-mini",
+        "thinking_effort": "high",
+        "temperature": 0.0,
+        "max_tokens": 10000,
+    },
+    "verification_test_coder": {
+        "provider": "openai",
+        "model_name": "gpt-5-mini",
+        "thinking_effort": "high",
+        "temperature": 0.0,
+        "max_tokens": 10000,
+    },
+}
 # ============================================================
-# End of model configuration
+# chat model configuration
+# ============================================================
+CHAT_TASK_SETTINGS: dict[str, dict[str, Any]] = {
+    "ocr": {
+        "provider": "mistral",
+        "model_name": "mistral-ocr-latest",
+        "include_image": False,
+        "table_format": "html",
+        # Following settings are applied only for local glm-ocr
+        "url": "http://localhost:11434",
+        "layout_device": "cpu",
+        "connection_pool_size": 1,
+        "max_workers": 1,
+        "batch_size": 1,
+    },
+    "chat": {
+        "provider": "ollama",
+        "is_batch": False,
+        "model_name": "qwen3:8b",
+        "thinking_effort": None,
+        "temperature": 0.0,
+        "max_tokens": 2000,
+    },
+    "identify_name": {
+        "provider": "ollama",
+        "is_batch": False,
+        "model_name": "qwen3:8b",
+        "thinking_effort": None,
+        "temperature": 0.0,
+        "max_tokens": 2000,
+    },
+}
+# ============================================================
+# END OF CONFIGURATION
 # ============================================================
 
-    for task_name, settings in model_settings.items():
-        task_config = getattr(config.task_configs, task_name)
+
+def configure_models(
+        preprocessing_config: PreprocessingConfig,
+        coding_config: CodingConfig,
+        chat_config: ChatConfig,
+) -> None:
+    """Configure model settings for the current runs."""
+
+    for task_name, settings in PREPROCESSOR_TASK_MODEL_SETTINGS.items():
+        if task_name == "ocr":
+            ocr_config = getattr(preprocessing_config,"ocr")
+            for field_name,value in settings.items():
+                setattr(ocr_config, field_name, value)
+            continue
+
+        task_config = getattr(preprocessing_config.task_configs, task_name)
 
         for field_name, value in settings.items():
             setattr(task_config.model, field_name, value)
 
+    for task_name, settings in CODING_TASK_MODEL_SETTINGS.items():
+        task_config = getattr(coding_config.task_configs, task_name)
 
-async def run_preprocessor(pdf_file:Path|str) -> None:
-    """Run the preprocessing pipeline for one PDF."""
+        for field_name, value in settings.items():
+            setattr(task_config.model, field_name, value)
 
-    pdf_file = Path(pdf_file)
-    env_file = Path(ENV_FILE)
+    for task_name, settings in CHAT_TASK_SETTINGS.items():
+        if task_name == "ocr":
+            ocr_config = getattr(chat_config,"ocr")
+            for field_name,value in settings.items():
+                setattr(ocr_config, field_name, value)
+            continue
+        task_config = getattr(chat_config.task_configs, task_name)
 
-    config = PreprocessingConfig.load_config(pdf=pdf_file,env=env_file)
-    configure_preprocessing_models(config)
+        for field_name, value in settings.items():
+            setattr(task_config.model, field_name, value)
 
-    preprocessor = Preprocessor(config)
-    await preprocessor.run()
+def main():
+    preprocessing_config = PreprocessingConfig.load_config(
+        pdf=PDF_FILE,
+        env=ENV_FILE,
+    )
+    coding_config = CodingConfig.load_config(
+        code_dir=CODE_DIR,
+        cli_path=CLI_PATH,
+        env=ENV_FILE,
+    )
+    chat_config = ChatConfig.load_config(
+        env=ENV_FILE,
+        database=DATABASE_PATH,
+    )
+
+    configure_models(
+        preprocessing_config=preprocessing_config,
+        coding_config=coding_config,
+        chat_config=chat_config,
+    )
+
+    device_name = DEVICE_NAME.strip()
+    match MODE.casefold():
+        case "chat":
+            start_chat(
+                chat_config=chat_config,
+                preprocessing_config=preprocessing_config,
+                coding_config=coding_config,
+            )
+
+        case "single_file":
+
+            if not device_name:
+                raise ValueError("DEVICE_NAME must not be empty.")
+
+            result = asyncio.run(
+                    run_preprocessor(
+                        config=preprocessing_config,
+                        pdf_path=Path(PDF_FILE),
+                        database_path=DATABASE_PATH,
+                        device_name=device_name,
+                    )
+            )
+
+            pages, register_map = get_latest_data(
+                database_path=Path(DATABASE_PATH),
+                device_name=result["device_name"],
+            )
+
+            asyncio.run(
+                run_coding_controller(
+                    config=coding_config,
+                    driver_name=result["device_name"],
+                    pages=pages,
+                    register_map=register_map,
+                    user_request=USER_REQUEST if USER_REQUEST else None,
+                )
+            )
+
+        case "multiple_files":
+            folder = Path(PDF_DIR)
+            if not folder.is_dir():
+                raise NotADirectoryError(f"Folder does not exist:{folder}")
+
+            pdf_files = sorted(folder.glob("*.pdf"))
+            device_names = [name.strip() for name in DEVICE_NAMES]
+
+            if not pdf_files:
+                raise FileNotFoundError(f"No PDF files found in:{folder}")
+            if len(pdf_files) != len(device_names):
+                raise ValueError(
+                    "PDF file count must match DEVICE_NAMES count."
+                )
+
+            if any(not name for name in device_names):
+                raise ValueError("DEVICE_NAMES must not contain empty names.")
+
+            for pdf_path, current_device_name in zip(
+                    pdf_files,
+                    device_names,
+                    strict=True,
+            ):
+                asyncio.run(
+                    run_preprocessor(
+                        config=preprocessing_config,
+                        pdf_path=pdf_path,
+                        database_path=DATABASE_PATH,
+                        device_name=current_device_name,
+                    )
+                )
+
+            unique_device_names = list(dict.fromkeys(device_names))
+            for name in unique_device_names:
+                pages, register_map = get_latest_data(
+                    database_path=Path(DATABASE_PATH),
+                    device_name=name
+                )
+
+                asyncio.run(
+                    run_coding_controller(
+                        config=coding_config,
+                        driver_name=name,
+                        pages=pages,
+                        register_map=register_map,
+                        user_request=USER_REQUEST if USER_REQUEST else None,
+                    )
+                )
+
+        case _:
+            raise ValueError(f"Unsupported MODE: {MODE}")
 
 
 if __name__ == "__main__":
-    match MODE:
-        case "single_test":
-            print(f"\n==============================")
-            print(f"Running tests for: {Path(PDF_FILE).stem}")
-            print(f"==============================\n")
-
-            # test_without_preprocessing(pdf_name)
-            asyncio.run(run_preprocessor(PDF_FILE))
-
-        case "multiple_test":
-            pdf_files = sorted(Path(PDF_DIR).glob("*.pdf"))
-
-            for pdf_file in pdf_files:
-                print(f"\n==============================")
-                print(f"Running tests for: {pdf_file.stem}")
-                print(f"==============================\n")
-
-                # test_without_preprocessing(pdf_name)
-                asyncio.run(run_preprocessor(pdf_file))
-
-
+    main()
