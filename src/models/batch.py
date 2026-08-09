1917588fe2e543ccf20fe54bfd41e5051263f419
yuying.chen <yuying.chen@tum.de>
2026-08-09 02:24:31 +0200

    "update"

--- src/models/batch.py
+++ src/models/batch.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 class UserRequest(BaseModel):
     custom_id:str
     user_input:str
+
+class RequestReference(BaseModel):
+    custom_id:str
+    pdf_sha256:str
+    page_index:int
